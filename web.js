c90dcc1036b79a16de9836a7563c00ef4fc7d375
Pitt Fagan <pittfagan@gmail.com>
2013-07-10 04:38:27 +0000

    change 5 to web.js

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var fs = require('fs');
--- var buf = require('buf');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var txt = fs.readFileSync('index.html');
-  var buftxt = new Buffer(txt,"utf-8");
-  response.send(buftxt);
+-- var txt = fs.readFileSync('index.html');
+  response.send(fs.readFileSync('index.html').toString());
+--  response.send(buftxt);
 --  response.send('Hello World 2!');
 });
 

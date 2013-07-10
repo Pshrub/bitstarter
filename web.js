60346e9d2d2ea07e315c1951faa76f175839efe8
Pitt Fagan <pittfagan@gmail.com>
2013-07-10 04:51:53 +0000

    change 8 to web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 -- var txt = fs.readFileSync('index.html');
-  response.send(fs.readFileSync('index.html').toString());
+   response.send(fs.readFileSync('index.html').toString());
 --  response.send(buftxt);
 --  response.send('Hello World 2!');
 });

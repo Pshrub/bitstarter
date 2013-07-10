6081a2bdca62634357047c6af2f4debbcf4902cc
Pitt Fagan <pittfagan@gmail.com>
2013-07-10 12:56:21 +0000

    change 10 to web.js

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
--- var txt = fs.readFileSync('index.html');
---   response.send(fs.readFileSync('index.html').toString());
-  response.send(txt);
+// var txt = fs.readFileSync('index.html');
+   response.send(fs.readFileSync('index.html').toString());
+// response.send(txt);
 });
 
 var port = process.env.PORT || 5000;
